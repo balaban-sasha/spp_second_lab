d09b23458dfe469dc23f67b08d73d8f5836e5061
MSI-PC <MSI-PC@DESKTOP-QESS1M1>
2016-09-18 18:05:58 +0300

    Work with 2 arguments.

---
+++ spp_lab_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spp_lab_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class WeakDelegate
+    {
+        private Action<int, int> ex1;
+
+        public WeakDelegate(Action<int, int> ex1)
+        {
+            this.ex1 = ex1;
+        }
+
+        public void GetDelegate()
+        {
+            ex1.Invoke(3,2);
+        }
+    }
+}
--- spp_second_lab/UnitTest1.cs
+++ spp_second_lab/UnitTest1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spp_lab_2;
+using System.Linq.Expressions;
 
-namespace spp_second_lab
+namespace spp_second_lab_tests
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +11,11 @@ namespace spp_second_lab
         [TestMethod]
         public void TestMethod1()
         {
+            Action<int, int> ex =
+        (x, y) => Console.WriteLine("Write {0} and {1}", x, y);
+            WeakDelegate weakDelegate = new WeakDelegate(ex);
+            weakDelegate.GetDelegate();
+
         }
     }
 }
