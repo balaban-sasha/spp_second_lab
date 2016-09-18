3152d22c9f41061c2394e6097b8a3421382a2e2f
MSI-PC <MSI-PC@DESKTOP-QESS1M1>
2016-09-18 17:28:14 +0300

    Добавьте файлы проекта.

---
+++ spp_second_lab/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace spp_second_lab
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
